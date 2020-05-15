985cf9b1fd035bc999534446afd0daf889b81df8
vladmany <vladmany99999@gmail.com>
2020-05-15 13:34:17 +0300

    изменил ограничение хранения файлов на сервере2

--- app/Http/Controllers/Api/PhotoController.php
+++ app/Http/Controllers/Api/PhotoController.php
@@ -38,7 +38,7 @@ class PhotoController extends Controller
      */
     public function store(PhotoRequest $request)
     {
-        if($this->getFilesSize(storage_path('app/public/images')) >= 250000000)
+        if($this->getFilesSize(storage_path('app/public/images')) >= 100000000)
         {
             return response()->json(array(
                 'code'      =>  401,
