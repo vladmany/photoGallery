101ca50765fc158a36b568b539bd0fc20585e440
Thorrnn <dmitry.gladchenko@gmail.com>
2020-05-15 13:15:14 +0300

    Ренейм альбома фикс

--- public/js/app.js
+++ public/js/app.js
@@ -67642,7 +67642,7 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vuex__WEBPACK_IMPORTED_MODULE_1__
 
       var commit = _ref2.commit,
           getters = _ref2.getters;
-      axios.post('/api/albums/update', {
+      axios.put('/api/albums/update', {
         id: this.state.IdAlbum,
         name: albumName
       }).then(function (response) {
