b0d377e4fe80070874ac61e4d8571630eeb061f0
krom9 <slonikzver1989@gmail.com>
2020-05-15 22:45:00 +0300

    доработка

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,4 +28,13 @@ class AuthServiceProvider extends ServiceProvider
 
         //
     }
+
+    protected function mapApiRoutes()
+    {
+        Route::prefix('api')
+            ->middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
+    }
+
 }
