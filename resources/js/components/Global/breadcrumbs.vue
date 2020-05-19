31851880b602779219526ad4b95b3b6f4522bc2f
vladmany <vladmany99999@gmail.com>
2020-05-20 01:08:54 +0300

    сделал изменение даты фото

---
+++ resources/js/components/Global/breadcrumbs.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p>{{ retStr }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "breadcrumbs",
+        props: {
+            crumbs: {      // { tile1: 'link1', title2: 'link2'} - что будет передаваться
+                type: Object,
+                required: true
+            }
+        },
+        computed: {
+            retStr() {
+                let breadStr = '';
+                for(let key of Object.keys(this.crumbs)) {
+                    breadStr += ''  // здесь наращиваешь крошки
+                                    // (ссылки(this.crumbs[key]) с титлам(key))
+                                    // c разделителем
+                }
+
+                return breadStr;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
