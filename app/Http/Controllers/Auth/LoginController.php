fbe4b550831da800780f61b2615777c00903ad19
krom9 <slonikzver1989@gmail.com>
2020-05-24 15:07:06 +0300

    доработка

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -102,7 +102,7 @@ class LoginController extends Controller
             Auth::login($myuser);
 
 //перекинуть в личны кабинет
-//            return response()->redirectTo(RouteServiceProvider::HOME);
+            return response()->redirectTo(RouteServiceProvider::HOME);
         }
     }
 
