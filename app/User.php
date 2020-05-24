ace13986c7de2616656d68d857d35f7f015d84c0
krom9 <slonikzver1989@gmail.com>
2020-05-24 15:38:34 +0300

    отладка

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'surname', 'password', 'remember_token',
+        'name', 'email', 'password', 'surname', 'remember_token', 'avatar_url'
     ];
 
     /**
