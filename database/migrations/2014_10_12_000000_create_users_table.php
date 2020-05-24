2309a17fe7b94c9cee02736c9651aac19c90dc6c
krom9 <slonikzver1989@gmail.com>
2020-05-24 15:44:56 +0300

    отладка

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->longText('remember_token');
+//            $table->rememberToken();
             $table->timestamps();
         });
     }
