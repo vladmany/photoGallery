c89aa95181f041a11429d582643e3589fdafc049
krom9 <slonikzver1989@gmail.com>
2020-05-24 16:44:00 +0300

    отладка

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -104,7 +104,7 @@ class LoginController extends Controller
             ]);
             Auth::login($myuser);
 
-//            dd($response ,$myuser);
+            dd($response ,$myuser);
 
 //перекинуть в личны кабинет
             return response()->redirectTo(RouteServiceProvider::HOME);
