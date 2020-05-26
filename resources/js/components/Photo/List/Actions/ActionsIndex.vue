5f99b339ff744bf86b7074d32499171ded6cf509
Thorrnn <dmitry.gladchenko@gmail.com>
2020-05-26 11:05:09 +0300

    Верстка ползунков, подскайзки панелик ФОТО, фикс бага при создании альбома

--- resources/js/components/Photo/List/Actions/ActionsIndex.vue
+++ resources/js/components/Photo/List/Actions/ActionsIndex.vue
@@ -2,18 +2,18 @@
     <div class="actions">
         <div class="action select_all" :class="(photos.length > 0) ? 'available' : ''">
             <input type="checkbox" class="custom-checkbox" id="all-selector" v-model="isSelected">
-            <label for="all-selector"></label>
+            <label for="all-selector" title="Выбрать все фото"></label>
         </div>
 <!--        <div class="action create">-->
 <!--            <object type="image/svg+xml" data="/storage/photos/actions/ic_create.svg"></object>-->
 <!--        </div>-->
-        <div class="action add_to_album" :class="isSelectedPhotos" @click="addToAlbum">
+        <div class="action add_to_album" :class="isSelectedPhotos" @click="addToAlbum" title="Добавить выбранные фото в альбом">
             <object type="image/svg+xml" data="/storage/photos/actions/ic_add_to_album.svg"></object>
         </div>
-        <div class="action download" :class="isSelectedPhotos" @click="download">
+        <div class="action download" :class="isSelectedPhotos" @click="download" title="Скачать выбранные фото">
             <object type="image/svg+xml" data="/storage/photos/actions/ic_download.svg"></object>
         </div>
-        <div class="action change_date" :class="isSelectedPhotos" @click="changeDate">
+        <div class="action change_date" :class="isSelectedPhotos" @click="changeDate" title="Изменить дату">
             <object type="image/svg+xml" data="/storage/photos/actions/ic_change_date.svg"></object>
         </div>
 <!--        <div class="action to_favorite" @click="toFavorite">-->
@@ -22,10 +22,10 @@
 <!--        <div class="action turn_image" @click="turnImage">-->
 <!--            <object type="image/svg+xml" data="/storage/photos/actions/ic_turn.svg"></object>-->
 <!--        </div>-->
-        <div class="action delete_image" :class="isSelectedPhotos" @click="deleteImages">
+        <div class="action delete_image" :class="isSelectedPhotos" @click="deleteImages" title="Удалить выбранные фото">
             <object type="image/svg+xml" data="/storage/photos/actions/ic_delete.svg"></object>
         </div>
-        <div class="action image_correction" :class="isSelectedPhotos1" @click="imageCorrection">
+        <div class="action image_correction" :class="isSelectedPhotos1" @click="imageCorrection" title="Открыть окно коррекции фото">
             <object type="image/svg+xml" data="/storage/photos/actions/ic_photo_correction.svg"></object>
         </div>
     </div>
