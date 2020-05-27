769b30d78920904e23b0ef49970e32129797db91
Thorrnn <dmitry.gladchenko@gmail.com>
2020-05-27 21:51:53 +0300

    Выравнивание альбомов

--- resources/js/components/Album/AlbumIndex.vue
+++ resources/js/components/Album/AlbumIndex.vue
@@ -106,6 +106,19 @@
         border-bottom: 2px solid #F5F5F5;
     }
 
+
+    @media (min-width: 768px) {
+        .main-panel {
+            justify-content: start;
+        }
+    }
+    @media (max-width: 767px) {
+        .main-panel {
+            justify-content: center;
+        }
+    }
+
+
     .buttons-edit {
         border-right-width: 2px;
         border-right-style: solid;
